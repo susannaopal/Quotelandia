04b5be3f3dd3dfd6cb4ead40448e41caba6581c6
Susanna Carey <susannaopal@gmail.com>
2022-03-02 12:29:25 -0500

    Remove console logs and unnecessary API call function

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Quotes from './Quotes';
-import { fetchQuotes, GetQuotes } from './apiCall.js'
+import { fetchQuotes } from './apiCall.js'
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      quotes: [
-    // {
-  //   "id": 1,
-  //   "title": "Genius is one percent inspiration and ninety-nine percent perspiration.",
-  //   "description": "Thomas Edison"
-  // },
-  // {
-  //   "id": 2,
-  //   "title": "You can observe a lot just by watching.",
-  //   "description": "Yogi Berra"
-  // },
-  // {
-  //   "id": 3,
-  //   "title": "A house divided against itself cannot stand.",
-  //   "description": "Abraham Lincoln"
-  // }
-  ]
+      quotes: []
     }
   }
 
 componentDidMount = () => {
   fetchQuotes()
-  // .then(data=>console.log("is this working?", data))
   .then(data => this.setState({quotes: data}))
-  // GetQuotes()
-  // .then(data=>console.log("are you also working?", data))
-
-
 }
  
 render() {
--- src/apiCall.js
+++ src/apiCall.js
@@ -1,12 +1,7 @@
 export const fetchQuotes = () => {
   return fetch('https://type.fit/api/quotes')
   .then(response => response.json())
-  // .then(data => console.log(data))
 }
 
-export const GetQuotes = () => {
-  return fetch('https://api.quotable.io/quotes')
-  .then(response => response.json())
-  // .then(data => console.log(data))
-}
+
 
