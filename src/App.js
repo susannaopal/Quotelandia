2944bdc309a3889c0aac9992decf89993df8d2db
Susanna Carey <susannaopal@gmail.com>
2022-03-02 17:22:36 -0500

    Update background for landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Quotes from './Quotes';
-import { fetchQuotes } from './apiCall.js'
+import Quotes from './Quotes.js';
+import { fetchQuotes } from './apiCall.js';
 
 
 class App extends Component {
@@ -12,16 +12,23 @@ class App extends Component {
     }
   }
 
-componentDidMount = () => {
-  fetchQuotes()
-  .then(data => this.setState({quotes: data}))
-}
+// componentDidMount = () => {
+//   fetchQuotes()
+//   .then(data => this.setState({quotes: data}))
+// }
  
 render() {
   return(
     <main className='App'>
-      <h1>Welcome to Quotelandia!</h1>
-      <Quotes quotes={this.state.quotes} />
+      <div className='header'>
+        <h1 className='header-title'>Welcome to Quotelandia</h1>
+      </div>
+        <img src="https://img.theculturetrip.com/wp-content/uploads/2017/10/old-books-436498_1280.jpg" alt='book-imagery-background' className='background-img' position='fixed'/>
+      <div className='nav-btns'>
+        <button className='left-side'>Quote of the Visit</button>
+        <button className='right-side'>Search for Author</button>
+      </div>
+      {/* <Quotes quotes={this.state.quotes} /> */}
     </main>
   )
 }
