0aae84c40746ffd34d61c9c9a9f03ae1d02a95b6
Susanna Carey <susannaopal@gmail.com>
2022-03-03 12:56:55 -0500

    Add together items for error messaging

--- src/Error.js
+++ src/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../CSS/Error.css'
+
 
 const Error = () => {
   return (
-    <div className='error'>
-        <p className='error-msg'>Oops, something went wrong!</p>
+    <div className='error-container'>
+        <p className='error-msg'>Oops, something is amiss!</p>
+              <img className='error-img' src={require('../Assets/cursive.png')} alt='error msg'/>
     </div>
   )
 }
