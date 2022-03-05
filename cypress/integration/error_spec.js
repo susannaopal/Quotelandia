376666529daa85ff72610f6947f922fd30038a3b
Susanna Carey <susannaopal@gmail.com>
2022-03-05 14:32:08 -0500

    Test for error handling with and without wifi

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -1,6 +1,6 @@
 describe('Error Flow', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://type.fit/api/quotes', {statusCode: 500})
+    cy.intercept('GET', 'https://type.fit/api/quotes', {statusCode: 500});
     cy.visit('http://localhost:3000/');
   });
 
@@ -9,8 +9,8 @@ describe('Error Flow', () => {
   });
 
   it('should show an error if a 404 error occurs', () => {
-    cy.intercept('GET', 'https://type.fit/api/quotes', {statusCode: 404})
+    cy.intercept('GET', 'https://type.fit/api/quotes', {statusCode: 404});
     cy.visit('http://localhost:3000/999999999999999');
     cy.contains('Oops, something is amiss! Please return home.');
-  })
-})
+  });
+});
