a35d9219033527bb5d7a94eb6db5974edf1a8b61
Susanna Carey <susannaopal@gmail.com>
2022-03-05 14:00:30 -0500

    Fix broken pathway links to configure component files, and remove footer to fix visuals

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import { fetchQuotes } from '../API/apiCall.js';
 import '../CSS/App.css';
 import Quotes from './Quotes.js';
-import Nav from '.Nav.js';
+import Nav from './Nav.js';
 import Search from './Search.js';
 import Home from './Home.js';
-import Footer from './Footer.js';
 // import Error from './Error.js';
 
 class App extends Component {
@@ -34,7 +33,6 @@ render() {
           <Route path='/quotes' element={<Quotes quotes={this.state.quotes} />}/>
           <Route path='/search' element={<Search quotes={this.state.quotes} />}/>
         </Routes>
-        <Footer />
     </main>
   )
   }
